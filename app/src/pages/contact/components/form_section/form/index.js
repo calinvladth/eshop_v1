7aed0e7e6783abd5efbd218097eb07b4fd3a4b8e
calinvlad <vlad.calin@grundriss.de>
2021-06-21 18:39:06 +0300

    ESV-55 Contact form validation

--- app/src/pages/contact/components/form_section/form/index.js
+++ app/src/pages/contact/components/form_section/form/index.js
@@ -10,9 +10,25 @@ import {useDispatch} from "react-redux";
 
 const FormComponent = () => {
     const [name, setName] = useState('')
+    const [nameError, setNameError] = useState({
+        show: false,
+        message: 'The name is required'
+    })
     const [email, setEmail] = useState('')
+    const [emailError, setEmailError] = useState({
+        show: false,
+        message: 'The email is required'
+    })
     const [subject, setSubject] = useState('')
+    const [subjectError, setSubjectError] = useState({
+        show: false,
+        message: 'The subject is required'
+    })
     const [message, setMessage] = useState('')
+    const [messageError, setMessageError] = useState({
+        show: false,
+        message: 'The message is required'
+    })
 
     const dispatch = useDispatch()
 
@@ -20,58 +36,106 @@ const FormComponent = () => {
 
     function submit(e) {
         e.preventDefault()
-        const data = {
-            name, email, subject, message
+        let pass = true
+
+        if (!name && name.length === 0) {
+            pass = false
+            setNameError({...nameError, show: true})
         }
-        axios({
-            method: 'POST',
-            url: `${api}/user_message/`,
-            data: {...data, shop: process.env.REACT_APP_SHOP_NAME}
-        })
-            .then(() => {
-                history.push(MessageSuccessPath)
-                setName('')
-                setEmail('')
-                setSubject('')
-                setMessage('')
+
+        if (!email && email.length === 0) {
+            pass = false
+            setEmailError({...emailError, show: true})
+        }
+
+        if (!subject && subject.length === 0) {
+            pass = false
+            setSubjectError({...subjectError, show: true})
+        }
+
+        if (!message && message.length === 0) {
+            pass = false
+            setMessageError({...messageError, show: true})
+        }
+
+        if (pass) {
+            const data = {
+                name, email, subject, message
+            }
+            axios({
+                method: 'POST',
+                url: `${api}/user_message/`,
+                data: {...data, shop: process.env.REACT_APP_SHOP_NAME}
             })
-            .catch(error => dispatch(SetAlert(error.response.data)))
+                .then(() => {
+                    history.push(MessageSuccessPath)
+                    setName('')
+                    setEmail('')
+                    setSubject('')
+                    setMessage('')
+                })
+                .catch(error => dispatch(SetAlert(error.response.data)))
+        } else {
+            document.getElementById('contactForm').scrollIntoView()
+        }
+
+
     }
 
     return (
-        <form className={style.form} onSubmit={(e) => submit(e)}>
+        <form id="contactForm" className={style.form} onSubmit={(e) => submit(e)}>
             <div className={style.formGroup}>
-                <input
-                    type="text"
-                    placeholder="Your name"
-                    className="input"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required/>
-                <input
-                    type="email"
-                    placeholder="Your email"
-                    className="input"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required/>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        className={`input ${nameError.show && name.length === 0 && "error"} ${nameError.show && name.length > 0 && "success"}`}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    {
+                        nameError.show && name.length === 0 && <span>{nameError.message}</span>
+                    }
+                </div>
+
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Your email"
+                        className={`input ${emailError.show && email.length === 0 && "error"} ${emailError.show && email.length > 0 && "success"}`}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {
+                        emailError.show && email.length === 0 && <span>{emailError.message}</span>
+                    }
+                </div>
+
+
             </div>
             <div>
                 <input
                     type="text"
                     placeholder="Subject"
-                    className="input"
+                    className={`input ${subjectError.show && subject.length === 0 && "error"} ${subjectError.show && subject.length > 0 && "success"}`}
                     value={subject}
                     onChange={(e) => setSubject(e.target.value)}
-                    required/>
+                />
+                {
+                    subjectError.show && subject.length === 0 && <span>{subjectError.message}</span>
+                }
+
             </div>
             <div>
                 <textarea
                     placeholder="Message here ..."
-                    className="textarea"
+                    className={`textarea ${messageError.show && message.length === 0 && "error"} ${messageError.show && message.length > 0 && "success"}`}
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
-                    required/>
+                />
+                {
+                    messageError.show && message.length === 0 && <span>{messageError.message}</span>
+                }
             </div>
             <div>
                 <button type="submit" className="button button--full-red">Submit</button>
